2a1ffbb0feb0724a78adcaf1a0ea251d6c89bc29
Patrick G. Santos <pgsantos.92@gmail.com>
2021-06-19 21:50:07 +0800

    Initial commit.

---
+++ app/components/Page.js
@@ -0,0 +1,13 @@
+import React, { useEffect } from "react"
+import Container from "./Container"
+
+function Page({ title, wide, children }) {
+  useEffect(() => {
+    document.title = `${title} | Complex App`
+    window.scrollTo(0, 0)
+  }, [])
+
+  return <Container wide={wide}>{children}</Container>
+}
+
+export default Page
