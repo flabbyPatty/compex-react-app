29b7cb73422654dc8be8c0ef42438fdad4617393
Patrick G. Santos <pgsantos.92@gmail.com>
2021-06-21 01:25:17 +0800

    Making the single post screen actully load real content.

--- app/components/Page.js
+++ app/components/Page.js
@@ -5,7 +5,7 @@ function Page({ title, wide, children }) {
   useEffect(() => {
     document.title = `${title} | Complex App`
     window.scrollTo(0, 0)
-  }, [])
+  }, [title])
 
   return <Container wide={wide}>{children}</Container>
 }
