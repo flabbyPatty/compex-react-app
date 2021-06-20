2eb4b974c84e44d985eee536251a42d5cd89445e
Patrick G. Santos <pgsantos.92@gmail.com>
2021-06-21 00:35:30 +0800

    Building the  profile screen.

--- app/Main.js
+++ app/Main.js
@@ -15,6 +15,7 @@ import Terms from "./components/Terms"
 import CreatePost from "./components/CreatePost"
 import ViewSinglePost from "./components/ViewSinglePost"
 import FlashMessages from "./components/FlashMessages"
+import Profile from "./components/Profile"
 
 import StateContext from "./StateContext"
 import DispatchContext from "./DispatchContext"
@@ -66,6 +67,9 @@ function Main() {
           <FlashMessages messages={state.flashMessages} />
           <Header />
           <Switch>
+            <Route path="/profile/:username">
+              <Profile />
+            </Route>
             <Route path="/" exact>
               {state.loggedIn ? <Home /> : <HomeGuest />}
             </Route>
