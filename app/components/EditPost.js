1d822daaa8581a910cf9ea0a228f39d4aae395a9
Patrick G. Santos <pgsantos.92@gmail.com>
2021-06-24 22:54:39 +0800

    Improving UX of editing/updating posts.

--- app/components/EditPost.js
+++ app/components/EditPost.js
@@ -1,11 +1,16 @@
-import React, { useState, useEffect } from "react"
+import React, { useContext, useEffect } from "react"
 import { useImmerReducer } from "use-immer"
 import { Link, useParams } from "react-router-dom"
 import Axios from "axios"
 import Page from "./Page"
 import LoadingDotsIcon from "./LoadingDotsIcon"
+import StateContext from "../StateContext"
+import DispatchContext from "../DispatchContext"
 
 function EditPost() {
+  const appState = useContext(StateContext)
+  const appDispatch = useContext(DispatchContext)
+
   const originalState = {
     title: {
       value: "",
@@ -18,7 +23,7 @@ function EditPost() {
       message: ""
     },
     isFetching: true,
-    savedIsLoading: false,
+    isSaving: false,
     id: useParams().id,
     sendCount: 0
   }
@@ -30,14 +35,33 @@ function EditPost() {
         draft.body.value = action.value.body
         draft.isFetching = false
         return
+      case "titleChange":
+        draft.title.value = action.value
+        return
+      case "bodyChange":
+        draft.body.value = action.value
+        return
+      case "submitRequest":
+        draft.sendCount++
+        return
+      case "saveRequestStarted":
+        draft.isSaving = true
+        return
+      case "saveRequestFinished":
+        draft.isSaving = false
+        return
     }
   }
 
   const [state, dispatch] = useImmerReducer(ourReducer, originalState)
 
+  function submitHandler(e) {
+    e.preventDefault()
+    dispatch({ type: "submitRequest" })
+  }
+
   useEffect(() => {
     const ourRequest = Axios.CancelToken.source()
-
     async function fetchPost() {
       try {
         const response = await Axios.get(`/post/${state.id}`, { cancelToken: ourRequest.token })
@@ -52,6 +76,26 @@ function EditPost() {
     }
   }, [])
 
+  useEffect(() => {
+    if (state.sendCount) {
+      dispatch({ type: "saveRequestStarted" })
+      const ourRequest = Axios.CancelToken.source()
+      async function updatePost() {
+        try {
+          const response = await Axios.post(`/post/${state.id}/edit`, { title: state.title.value, body: state.body.value, token: appState.user.token }, { cancelToken: ourRequest.token })
+          dispatch({ type: "saveRequestFinished" })
+          appDispatch({ type: "flashMessage", value: "Post was updated." })
+        } catch (e) {
+          console.log(e)
+        }
+      }
+      updatePost()
+      return () => {
+        ourRequest.cancel()
+      }
+    }
+  }, [state.sendCount])
+
   if (state.isFetching)
     return (
       <Page title="...">
@@ -61,22 +105,24 @@ function EditPost() {
 
   return (
     <Page title={"Edit Post"}>
-      <form>
+      <form onSubmit={submitHandler}>
         <div className="form-group">
           <label htmlFor="post-title" className="text-muted mb-1">
             <small>Title</small>
           </label>
-          <input value={state.title.value} autoFocus name="title" id="post-title" className="form-control form-control-lg form-control-title" type="text" placeholder="" autoComplete="off" />
+          <input onChange={e => dispatch({ type: "titleChange", value: e.target.value })} value={state.title.value} autoFocus name="title" id="post-title" className="form-control form-control-lg form-control-title" type="text" placeholder="" autoComplete="off" />
         </div>
 
         <div className="form-group">
           <label htmlFor="post-body" className="text-muted mb-1 d-block">
             <small>Body Content</small>
           </label>
-          <textarea name="body" id="post-body" className="body-content tall-textarea form-control" type="text" value={state.body.value} />
+          <textarea onChange={e => dispatch({ type: "bodyChange", value: e.target.value })} name="body" id="post-body" className="body-content tall-textarea form-control" type="text" value={state.body.value} />
         </div>
 
-        <button className="btn btn-primary">Save Updates</button>
+        <button className="btn btn-primary" disabled={state.isSaving}>
+          {state.isSaving ? "Saving..." : "Save Updates"}
+        </button>
       </form>
     </Page>
   )
